6db16e5f1feb2a8f378eba0d87a17d5292094027
bugcodesource <34241126+bugcodesource@users.noreply.github.com>
2021-01-24 14:59:59 +0000

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -600,7 +600,7 @@ export default {
   mounted() {
     this.form.clientType = "clash&new_name=true";
     this.form.customBackend = "https://sub.corespace.space/sub?";
-    this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
+    this.form.remoteConfig = "https://raw.githubusercontent.com/bugcodesource/now-subconverter/master/subconverter/config/ACL4SSR_ZiYong_Full_AdblockPlus.ini";
     this.getBackendVersion();
   },
   methods: {
